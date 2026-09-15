b97046792d85857e223d64f9587735324c1f054b
Аксенов ЦКБ-141 <romatvink3000@gmail.com>
2026-09-15 19:20:41 +0300

    Create 1-2.c

---
+++ 1-2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+/
+ * @brief рассчивает прееменную b
+ * @param a - первая сторона прямоугольника
+ * @param n - коэффициент
+ * @return расчитанное значение
+ */
+double B( const double a, const double n);
+
+/
+ * @brief точка входа в программу
+ * @return значение 0, если программа выполнена корректно
+ */
+int main() {
+  double a(), n();
+
+  printf("Введите значение первой стороны: ");
+  scanf("%lf", &a);
+  printf("Введите значение коэффициента (%%): ");
+  scanf("%lf", &n);
+
+  n /= 100;
+
+  double perimetr = (a + B(a, n)) * 2;
+  double area = a * B(a, n);
+
+  printf("Площадь прямоугольника = %lf\n", area);
+  printf("Периметр прямоугольника = %lf\n", perimetr);
+
+  return 0;
+}
+
+double B(const double a, const double n) {
+  return (a * n);
+}
