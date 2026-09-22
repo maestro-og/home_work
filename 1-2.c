7c45e9d584166a27237f9e46ed608d2ffef86267
Аксенов ЦКБ-141 <romatvink3000@gmail.com>
2026-09-22 21:46:01 +0300

    Rename checkparam function to Checkparam

--- 1-2.c
+++ 1-2.c
@@ -13,7 +13,7 @@ double GETvalue();
  * @brief проверяет значеие на положительность
  * @param a - проверяемое значение
  */
-void checkparam(const double a);
+void Checkparam(const double a);
 /**
  * @brief рассчивает вторую сторону
  * @param a - первая сторона прямоугольника
@@ -47,11 +47,11 @@ int main() {
 
     printf("Введите значение первой стороны: ");
     double side1 = GETvalue();
-    checkparam(side1);
+    Checkparam(side1);
 
     printf("Введите значение коэффициента (%%): ");
     double kef = GETvalue();
-    checkparam(kef);
+    Checkparam(kef);
 
     kef /= 100;
 
@@ -70,7 +70,7 @@ double GETvalue(){
     return value;
 }
 
-void checkparam(const double a){
+void Checkparam(const double a){
     if (a <= 0){
         printf("Значение строго положительное\n");
         exit(1);
