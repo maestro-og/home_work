dc73d68003a1986073becebb6c5fbe0ee946d62a
Аксенов ЦКБ-141 <romatvink3000@gmail.com>
2026-09-22 21:37:24 +0300

    Refactor rectangle calculations and input handling

--- 1-2.c
+++ 1-2.c
@@ -1,38 +1,90 @@
+
 #include <stdio.h>
 #include <math.h>
+#include <locale.h>
+#include <stdlib.h>
+/**
+ * @brief считывает число + проверка на корректность ввода
+ * @return возвращает число пользователя , иначе выводит ошибку
+ */
+double GETvalue();
 
+/**
+ * @brief проверяет значеие на положительность
+ * @param a - проверяемое значение
+ */
+void checkparam(const double a);
 /**
  * @brief рассчивает вторую сторону
  * @param a - первая сторона прямоугольника
  * @param n - коэффициент
  * @return расчитанное значение
  */
-double B( const double a, const double n);
+double Side2(const double a, const double n);
+
+ /**
+  * @brief вычисляет периметр прямоугольника
+  * @param a - первая сторона
+  * @param b - вторая сторона
+  * @return значение периметра
+  */
+double Perimetr(const double a, const double b);
+
+/**
+ * @brief вычисляет площадь прямоугольника
+ * @param a - первая сторона
+ * @param b - вторая сторона
+ * @return значение площади
+ */
+double Area(const double a, const double b);
 
 /**
  * @brief точка входа в программу
  * @return значение 0, если программа выполнена корректно
  */
 int main() {
-  double a = 0;
-  double n = 0;
+    setlocale(LC_ALL, "Russian");
 
-  printf("Введите значение первой стороны: ");
-  scanf("%lf", &a);
-  printf("Введите значение коэффициента (%%): ");
-  scanf("%lf", &n);
+    printf("Введите значение первой стороны: ");
+    double side1 = GETvalue();
+    checkparam(side1);
 
-  n /= 100;
+    printf("Введите значение коэффициента (%%): ");
+    double kef = GETvalue();
+    checkparam(kef);
 
-  double perimetr = (a + B(a, n)) * 2;
-  double area = a * B(a, n);
+    kef /= 100;
 
-  printf("Площадь прямоугольника = %lf\n", area);
-  printf("Периметр прямоугольника = %lf\n", perimetr);
+    printf("Площадь прямоугольника = %lf\n", Area(side1, Side2(side1, kef)));
+    printf("Периметр прямоугольника = %lf\n", Perimetr(side1, Side2(side1, kef)));
 
-  return 0;
+    return 0;
 }
 
-double B(const double a, const double n) {
-  return (a * n);
+double GETvalue(){
+    double value = 0;
+    if (!scanf("%lf",&value)){
+        printf("Error\n");
+        exit(1);
+    }
+    return value;
+}
+
+void checkparam(const double a){
+    if (a <= 0){
+        printf("Значение строго положительное\n");
+        exit(1);
+    }
+}
+
+double Side2(const double a, const double n) {
+    return (a * n);
+}
+
+double Perimetr(const double a, const double b) {
+    return (a + b) * 2;
+}
+
+double Area(const double a, const double b) {
+    return a * b;
 }
